bfb03ec93dc5a063b25444ee59f454b5bb962c38
NIDIGANTIVIJAY <80302503+NIDIGANTIVIJAY@users.noreply.github.com>
2023-08-18 08:29:24 +0530

    Add files via upload

---
+++ build/src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Headers from "./components/jsFile/Header";
+import Headers1 from "./components/jsFile/Header1";
+import Home from "./components/jsFile/Home";
+import Product from "./components/jsFile/Product";
+import Sales from "./components/jsFile/Sales";
+import AdminDashboard from "./components/jsFile/AdminDashboard";
+import Footer from "./components/jsFile/Footer";
+import Contact from "./components/jsFile/Contact";
+import FAQ from "./components/jsFile/Faq";
+import About from "./components/jsFile/About";
+import Auth from "./components/jsFile/Auth";
+import Header from './components/jsFile/Header';
+// import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route ,Outlet} from "react-router-dom";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import ContainerPage from "./components/jsFile/index" 
+function App() {
+  // const refresh = () => window.location.reload(true)
+
+  const location = useLocation();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loggedInFun = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
+
+  return (
+    <>
+    <Header/>
+      
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/faq" element={<FAQ />} />
+        <Route exact path="/sales" element={<Sales />} />
+        <Route exact path="/adminDashboard" element={<AdminDashboard />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/product" element={<Product />} />
+        <Route exact path="/login" element={<Auth />} />
+        
+        </Routes>
+      
+
+      <div>
+        <Footer />
+      </div>
+
+
+      {/* <Outlet/> */}
+    </>
+  );
+}
+
+export default App;
---
+++ build/src/components/jsFile/Faq.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from '../cssFile/Faq.module.css';
+
+const Faq = () => {
+  return (
+    <main className={classes.profile}>
+      <h2>Frequently Ask Question </h2>
+      <p>snjhs jhjwnjsnwkk kwjkw kkwjjkw kkwjjkw</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium ipsam? Voluptatum perspiciatis in aliquam, vitae hic modi totam sint nisi autem nobis maiores quas odio voluptate maxime saepe ducimus.</p>
+    </main>
+  );
+};
+
+export default Faq;
